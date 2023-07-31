311083d7d32d257e7aa5cad30b98c254c4c62404
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-07-31 15:15:15 +0200

    app

--- src/components/AgeDashdoard/AgeDashdoard.jsx
+++ src/components/AgeDashdoard/AgeDashdoard.jsx
@@ -4,27 +4,32 @@ import { StyledSpan, StyledText, StyledWrapper } from './AgeDashdoard.styled';
 const AgeDashdoard = ({ data }) => {
   return (
     <>
-      {Object.keys(data).length !== 0 && (
-        <StyledWrapper>
-          <div>
-            <StyledText>
-              <StyledSpan>{formatNumberToTwoDigits(data.years)}</StyledSpan>
-              Years
-            </StyledText>
-          </div>
-          <div>
-            <StyledText>
-              <StyledSpan>{formatNumberToTwoDigits(data.months)}</StyledSpan>
-              Months
-            </StyledText>
-          </div>
-          <div>
-            <StyledText>
-              <StyledSpan>{formatNumberToTwoDigits(data.days)}</StyledSpan>Days
-            </StyledText>
-          </div>
-        </StyledWrapper>
-      )}
+      <StyledWrapper>
+        <div>
+          <StyledText>
+            <StyledSpan>
+              {data.years ? formatNumberToTwoDigits(data.years) : '--'}
+            </StyledSpan>
+            Years
+          </StyledText>
+        </div>
+        <div>
+          <StyledText>
+            <StyledSpan>
+              {data.months ? formatNumberToTwoDigits(data.months) : '--'}
+            </StyledSpan>
+            Months
+          </StyledText>
+        </div>
+        <div>
+          <StyledText>
+            <StyledSpan>
+              {data.days ? formatNumberToTwoDigits(data.days) : '--'}
+            </StyledSpan>
+            Days
+          </StyledText>
+        </div>
+      </StyledWrapper>
     </>
   );
 };
